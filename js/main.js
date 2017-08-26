8bef68cce8fade494cd287cffe5da36827433a6f
Knut Olav <tyfonknut@gamil.com>
2017-08-26 20:45:05 +0200

    Made a lot of experimentation, and removed said experimentation. Learned a lot. Removed unnecessary for functionality and added functions. Removed styling, grids, and others. Since I werent able to properly use these styling, I have removed them. Reasoning being, to continue on other material.

--- js/main.js
+++ js/main.js
@@ -2,98 +2,108 @@
 
 const TWITCH_EMBED = $("#twitch-embed");
 const TWITCH_STREAMERS = $("#twitch-streamers");
-const TWITCH_SEARCH = $("#twitch-search")
+const TWITCH_SEARCH = $("#twitch-search");
 
 //Variables, URL or other resources.
 let streamerArray = ["esl_csgo","geekygoonsquad","nocopyrightsounds","tejbz","shroud","summit1g"];
 
-var twitchUrl = "http://player.twitch.tv/?channel=monstercat&autoplay=false";
 var baseTwitchUrl = "https://wind-bow.gomix.me/twitch-api/streams/";
-
-
-function checkAndInsertData(data,user){
-  var insertString = ""
-
-  if(data.stream == null){
-    insertString += offlineString(data._links, user);
-  }
-  else{
-    insertString += onlineString(data.stream,data._links,user)
-  }
-    TWITCH_STREAMERS.append(insertString);
-
-}
-
-function insertFrame(element,url){
-  element.html('<iframe src="'+ url +'"></iframe>');
-}
-
-function resizeEmbeddedPlayer(object){
-  object.height(object.width() / 16 * 9 + 'px');
-}
+var endTwitchUrl = "?callback=?";
 
 
 window.onload = function(){
-  insertFrame(TWITCH_EMBED,twitchUrl);
-  resizeEmbeddedPlayer(TWITCH_EMBED);
-
   fetchTwitchUsers(streamerArray);
 }
 
 function fetchTwitchUsers(array){
-  TWITCH_STREAMERS.html("");
-  for(var a in array){
-    getData(array[a]);
-  }
-}
-
-function getData( user){
-  let searchURL = baseTwitchUrl + user;
-  $.ajax({
-    url: searchURL,
-    dataType: "jsonp",
-    success: function(data){
-      checkAndInsertData(data,user);
-      console.log(JSON.stringify(data))
+  let currentArray = [];
+  
+    for(var a in array){
+      let user = array[a];
+      let searchURL = baseTwitchUrl + array[a] + endTwitchUrl;
+      $.ajax({
+        url: searchURL,
+        dataType: "jsonp",
+        success: function(data){
+           currentArray.push(checkAndInsertData(data,user));
+           insertinDome(currentArray);
+          }
+      });
     }
-  });
 }
 
-function checkAndInsertData(data,user){
-  var insertString = ""
+function insertinDome(array){
+  tempArray= array.sort(function(b,a){
+    if(a.getOnline() == true && b.getOnline()==false){
+      return 1;
+    }
+  
+    if(a.getOnline() == false && b.getOnline()==true){
+      return -1;
+    }
+    
+    return 0;
+  });
+  TWITCH_STREAMERS.html("");
+  for(var a in tempArray){
+    var player = array[a];
+    TWITCH_STREAMERS.append('<article><a href="'+ player.getTwitch() +
+                            '" target="_blank">' 
+                            +player.getName() + '<p>'
+                            +player.getGame() + '</p><div class="row">'
+                            +offlineOrOffline(player.getOnline()) 
+                            +'</a></article>');
+  }
+}
+function offlineOrOffline(boolean){
+  if(boolean == true){
+    return '<div class="online">Online</div>';
+  }
+  else {
+    return '<div class="offline">Offline</div>';
+  }
+}
 
+function findThisGame(data){
   if(data.stream == null){
-    insertString += offlineString(data._links,user);
+    return "";
   }
-  else{
-    insertString += onlineString(data.stream,data._links,user)
+  return data.stream.game;
+}
+
+
+function checkAndInsertData(data, user){
+  var name = user;
+  var streamer = Object.create(streamerObject);
+  var game = findThisGame(data);
+  streamer.init(name,data.stream,game)
+  return streamer;
+}
+
+var streamerObject = {
+  init: function(name,status,game){
+    this.name = name;
+    this.status = status || false;
+    this.game = game;
+  },
+  getName: function(){
+    return this.name;
+  },
+  getStatus: function(){
+    return this.status;
+  },
+  getGame: function(){
+    return this.game;
+  },
+  getTwitch: function(){
+    return "https://www.twitch.tv/"+ this.name;
+  },
+  getOnline: function(){
+    if(this.status == false){
+      return false;
+    }
+    else{
+      return true;
+    }
   }
-    TWITCH_STREAMERS.append(insertString);
-    document.getElementById(user).onclick = function(){
-      insertFrame(TWITCH_EMBED,getTwitchPlayerURL(user));
-    };
-}
-
-function getTwitchPlayerURL(name){
-  return "http://player.twitch.tv/?channel="+ name +"&autoplay=false";
-}
-
-var resizeEvent = window.addEventListener('resize',function(){
-  resizeEmbeddedPlayer(TWITCH_EMBED);
-});
-
-function offlineString(data,name){
-  return '<article><a href="'+ data.channel +'"></a><h1>'+ name +'</h1>' +
-  '<div class="row"><div class="btn"><a href="https://www.twitch.tv/' + name  +'" target="_blank">View on twitch</a></div> <div class="btn" id="'+ name +'">View here</div></div> <div class="twitcher_status offline">Offline</div><article>';
-} 
-
-function onlineString(stream,data,name){
-  return '<article><h1>'+ name +'</h1><p>'+ stream.game +'</p>'+
-         '<div class="row"><div class="btn"><a href="https://www.twitch.tv/' + name  +'" target="_blank">View on twitch</a></div><div class="btn" id="'+ name +'">View here</div></div> <div class="twitcher_status online">Online</div></article>';
-}
-
-function sortArray(array){
-  return array.sort(function(a, b){
-    return a.localeCompare(b);
-  });
-}
+};
